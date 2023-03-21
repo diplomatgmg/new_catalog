72fffcc24882272d430d08ed26e77056406afde5
Кирилл Попов <diplomatgmg@gmail.com>
2023-03-22 00:31:30 +0300

    Запросы в БД уменьшены до 4

--- apps/product/base_views.py
+++ apps/product/base_views.py
@@ -1,77 +1,79 @@
 from django.views.generic import ListView
-
 from apps.product.models import Brand
+from django.db.models import Min, Max
 
 
 class BaseProductListView(ListView):
     template_name = 'product/product-list.html'
     context_object_name = 'products'
-    range_filter_fields = dict()
-    search_filter_fields = tuple()
+    range_filter_fields = {}
+    search_filter_fields = ()
 
     def get_queryset(self):
         query = self.request.GET.get('q')
         queryset = self.model.objects.all().select_related('brand')
 
-        # Фильтруем queryset строго
         if query:
-            # Очищаем запрос от лишних пробелов
             query = '.*' + ' '.join(query.lower().strip().split()) + '.*'
             query_new = query.replace(' ', '.')
             queryset_new = queryset.filter(slug__iregex=query_new)
-
-            # Если ничего не находим, фильтруем не строго
             if not queryset_new:
                 query_new = query.replace(' ', '.+')
                 queryset = queryset.filter(slug__iregex=query_new)
             else:
                 queryset = queryset_new
 
-        # Фильтрация бренда
         brand = self.request.GET.getlist('brand')
         if brand:
             queryset = queryset.filter(brand__name__in=brand)
 
-        # Фильтрация диапазона значений
-        for field_name, filter_name in self.range_filter_fields.items():
-            filter_value_min = self.request.GET.get(f'min_{filter_name}')
-            filter_value_max = self.request.GET.get(f'max_{filter_name}')
-            if filter_value_min and filter_value_max:
-                queryset = queryset.filter(**{
-                    f'{field_name}__range': (filter_value_min, filter_value_max),
-                })
-            elif filter_value_min:
-                queryset = queryset.filter(**{
-                    f'{field_name}__gte': filter_value_min,
-                })
-            elif filter_value_max:
-                queryset = queryset.filter(**{
-                    f'{field_name}__lte': filter_value_max,
-                })
+        for field_name in self.range_filter_fields:
+            filter_value_min = self.request.GET.get(f'min_{field_name}')
+            filter_value_max = self.request.GET.get(f'max_{field_name}')
+            filter_kwargs = {}
+            if filter_value_min:
+                filter_kwargs[f'{field_name}__gte'] = filter_value_min
+            if filter_value_max:
+                filter_kwargs[f'{field_name}__lte'] = filter_value_max
+            if filter_kwargs:
+                queryset = queryset.filter(**filter_kwargs)
 
-        # Фильтрация поиска
         for field in self.search_filter_fields:
             value = self.request.GET.getlist(field)
             if value:
                 queryset = queryset.filter(**{f'{field}__in': value})
 
-        self.queryset = queryset
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['brands'] = self.get_brand_names()
 
+        context['disabled_brands'] = []
+        context['brands'] = []
+
+        for brand in set(self.model.objects.values_list('brand__name', flat=True)):
+            if brand in set(product.brand.name for product in self.object_list):
+                context['brands'].append(brand)
+            else:
+                context['disabled_brands'].append(brand)
+
+
+
+        # context['brands'] = list(Brand.objects.filter(id__in=model_brands).order_by('name').values_list('name', flat=True))
+        #
+        # Для обработки поиска js
         context['search_fields'] = self.search_filter_fields
-        # Добавить метод .distinct('field_name') для получения множества после перехода на PostgreSQL
-        for field in self.search_filter_fields:
-            context[f'{field}_search_list'] = sorted(set(self.queryset.values_list(field, flat=True)))
 
-        for field in self.range_filter_fields:
-            context[field] = self.queryset.values_list(field, flat=True)
+        if self.object_list.exists():
+            for field_name in self.search_filter_fields:
+                context[f'{field_name}_search_list'] = sorted(set(getattr(product, field_name) for product in self.object_list))
+
+            for field_name in self.range_filter_fields:
+                context[f'{field_name}_min'] = min(getattr(product, field_name) for product in self.object_list)
+                context[f'{field_name}_max'] = max(getattr(product, field_name) for product in self.object_list)
+
+        #
+        # for field in self.range_filter_fields:
+        #     context[field] = self.queryset.values_list(field, flat=True)
 
         return context
-
-    def get_brand_names(self):
-        model_brands = set(self.model.objects.values_list('brand', flat=True))
-        return [brand.name for brand in Brand.objects.filter(id__in=model_brands).order_by('name')]
--- apps/product/views.py
+++ apps/product/views.py
@@ -6,28 +6,21 @@ class CPUListView(BaseProductListView):
     template_name = 'product/includes/cpu-list.html'
     model = CPUModel
     # Поля для фильтрации диапазона
-    range_filter_fields = {
-        'num_cores': 'num_cores',
-        'base_clock': 'base_clock',
-    }
+    range_filter_fields = (
+        'num_cores',
+        'base_clock',
+    )
 
     search_filter_fields = (
         'family',
         'model',
     )
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-
-
-
-        return queryset
-
 
 class GPUListView(BaseProductListView):
     template_name = 'product/includes/gpu-list.html'
     model = GPUModel
-    range_filter_fields = {
-        'base_clock': 'base_clock',
-        'boost_clock': 'boost_clock',
-    }
+    range_filter_fields = (
+        'base_clock',
+        'boost_clock',
+    )
