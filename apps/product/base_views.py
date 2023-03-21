ed06a0c3e292111683f844bb0b1d942bb852d450
Кирилл Попов <diplomatgmg@gmail.com>
2023-03-21 15:07:53 +0300

    Упрощен queryset в представлении моделей

--- apps/product/base_views.py
+++ apps/product/base_views.py
@@ -6,7 +6,8 @@ from apps.product.models import Brand
 class BaseProductListView(ListView):
     template_name = 'product/product-list.html'
     context_object_name = 'products'
-    range_filter_fields = None
+    range_filter_fields = dict()
+    search_filter_fields = tuple()
 
     def get_queryset(self):
         query = self.request.GET.get('q')
@@ -33,20 +34,26 @@ class BaseProductListView(ListView):
 
         # Фильтрация диапазона значений
         for field_name, filter_name in self.range_filter_fields.items():
-            filter_value_min = self.request.GET.get(f"min_{filter_name}")
-            filter_value_max = self.request.GET.get(f"max_{filter_name}")
+            filter_value_min = self.request.GET.get(f'min_{filter_name}')
+            filter_value_max = self.request.GET.get(f'max_{filter_name}')
             if filter_value_min and filter_value_max:
                 queryset = queryset.filter(**{
-                    f"{field_name}__range": (filter_value_min, filter_value_max),
+                    f'{field_name}__range': (filter_value_min, filter_value_max),
                 })
             elif filter_value_min:
                 queryset = queryset.filter(**{
-                    f"{field_name}__gte": filter_value_min,
+                    f'{field_name}__gte': filter_value_min,
                 })
             elif filter_value_max:
                 queryset = queryset.filter(**{
-                    f"{field_name}__lte": filter_value_max,
+                    f'{field_name}__lte': filter_value_max,
                 })
+
+        # Фильтрация поиска
+        for field in self.search_filter_fields:
+            value = self.request.GET.getlist(field)
+            if value:
+                queryset = queryset.filter(**{f'{field}__in': value})
 
         self.queryset = queryset
         return queryset
@@ -54,6 +61,15 @@ class BaseProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['brands'] = self.get_brand_names()
+
+        context['search_fields'] = self.search_filter_fields
+        # Добавить метод .distinct('field_name') для получения множества после перехода на PostgreSQL
+        for field in self.search_filter_fields:
+            context[f'{field}_search_list'] = sorted(set(self.queryset.values_list(field, flat=True)))
+
+        for field in self.range_filter_fields:
+            context[field] = self.queryset.values_list(field, flat=True)
+
         return context
 
     def get_brand_names(self):
--- apps/product/views.py
+++ apps/product/views.py
@@ -7,38 +7,27 @@ class CPUListView(BaseProductListView):
     model = CPUModel
     # Поля для фильтрации диапазона
     range_filter_fields = {
-        "num_cores": "num_cores",
-        "base_clock": "base_clock",
+        'num_cores': 'num_cores',
+        'base_clock': 'base_clock',
     }
+
+    search_filter_fields = (
+        'family',
+        'model',
+    )
 
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        family = self.request.GET.getlist('family')
-        if family:
-            queryset = queryset.filter(family__in=family)
+
 
         return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['family_list'] = set(self.queryset.values_list('family', flat=True))
-        context['num_cores'] = set(self.queryset.values_list('num_cores', flat=True))
-        context['base_clock'] = set(self.queryset.values_list('base_clock', flat=True))
-
-        return context
 
 
 class GPUListView(BaseProductListView):
     template_name = 'product/includes/gpu-list.html'
     model = GPUModel
     range_filter_fields = {
-        "base_clock": "base_clock",
-        "boost_clock": "boost_clock",
+        'base_clock': 'base_clock',
+        'boost_clock': 'boost_clock',
     }
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['base_clock'] = set(self.queryset.values_list('base_clock', flat=True))
-        context['boost_clock'] = set(self.queryset.values_list('boost_clock', flat=True))
-        return context
